361eccbae1673c503aceee3e3bdab5432588a389
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2022-07-02 16:09:43 -0400

    Updated servoMotor.js

--- docs/04motors/code/servoMotor.js
+++ docs/04motors/code/servoMotor.js
@@ -1,29 +1,38 @@
 #!/usr/bin/env node
+////////////////////////////////////////
+//	servoMotor.js
+//	Drive a simple servo motor back and forth on P9_16 pin
+//	Wiring:
+//	Setup:  config-pin P9_16 pwm
+//          cd /sys/class/pwm/pwmchip5
+//          echo 1 > export
+//          cd pwm1
+//          chgrp Blinks thegpio *
+//          chmod g+w *
+//	See:
+////////////////////////////////////////
+const fs = require("fs");
 
-// Drive a simple servo motor back and forth
-
-var b = require('bonescript');
-
-var motor = 'P9_21', // Pin to control servo
-    freq = 50,  // Servo frequency (20 ms)
-    min  = 0.8, // Smallest angle (in ms)
-    max  = 2.5, // Largest angle (in ms)
-    ms  = 250,  // How often to change position, in ms
-    pos = 1.5,  // Current position, about middle
-    step = 0.1; // Step size to next position
+const pwmPeriod = '20000000';    // Period in ns, (20 ms)
+const pwmchip = '5';  // pwm chip to use
+const pwm = '1';      // pwm to use
+const PWMPATH='/sys/class/pwm/pwmchip'+pwmchip;
+const min  = 0.8, // Smallest angle (in ms)
+      max  = 2.4, // Largest angle (in ms)
+      ms  = 250;  // How often to change position, in ms
+var   pos = 1.5,  // Current position, about middle ms)
+      step = 0.1; // Step size to next position
 
 console.log('Hit ^C to stop');
-b.pinMode(motor, b.ANALOG_OUTPUT, 6, 0, 0, doInterval);
+// fs.writeFileSync(PWMPATH+'/export', pwm);   // Export the pwm channel
+// Set the period in ns, first 0 duty_cycle, 
+fs.writeFileSync(PWMPATH+'/pwm'+pwm+'/duty_cycle', '0');
+fs.writeFileSync(PWMPATH+'/pwm'+pwm+'/period', pwmPeriod);
+fs.writeFileSync(PWMPATH+'/pwm'+pwm+'/duty_cycle', pwmPeriod/2);
+fs.writeFileSync(PWMPATH+'/pwm'+pwm+'/enable', '1');
 
-function doInterval(x) {
-    if(x.err) {
-        console.log('x.err = ' + x.err);
-        return;
-    }
-    timer = setInterval(sweep, ms);
-}
-
-move(pos);      // Start in the middle
+var timer = setInterval(sweep, ms);
+// move(pos);      // Start in the middle
 
 // Sweep from min to max position and back again
 function sweep() {
@@ -35,13 +44,21 @@ function sweep() {
 }
 
 function move(pos) {
-    var dutyCycle = pos/1000*freq;
-    b.analogWrite(motor, dutyCycle, freq);
-    console.log('pos = ' + pos + ' duty cycle = ' + dutyCycle);
+    var dutyCycle = parseInt(pos*1000000);    // Convert ms to ns
+    // console.log('pos = ' + pos + ' duty cycle = ' + dutyCycle);
+    fs.writeFileSync(PWMPATH+'/pwm'+pwm+'/duty_cycle', dutyCycle);
 }
 
 process.on('SIGINT', function() {
     console.log('Got SIGINT, turning motor off');
     clearInterval(timer);             // Stop the timer
-    b.analogWrite(motor, 0, freq);    // Turn motor off
+    fs.writeFileSync(PWMPATH+'/pwm'+pwm+'/enable', '0');
 });
+
+// | Pin   | pwmchip | pwm
+// | P9_31 | 3       | 0
+// | P9_29 | 3       | 1
+// | P9_14 | 5       | 0
+// | P9_16 | 5       | 1
+// | P8_19 | 7       | 0
+// | P8_13 | 7       | 1
