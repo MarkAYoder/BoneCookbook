c97097f260616f484e6d003e1b5499d1ed926179
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2022-07-05 14:54:24 -0400

    dcMotor.js is bs free

--- docs/04motors/code/dcMotor.js
+++ docs/04motors/code/dcMotor.js
@@ -1,37 +1,50 @@
 #!/usr/bin/env node
+////////////////////////////////////////
+//	dcMotor.js
+//	This is an example of driving a DC motor
+//	Wiring:
+//	Setup:  config-pin P9_16 pwm
+//          cd /sys/class/pwm/pwmchip5
+//          echo 1 > export
+//          cd pwm1
+//          chgrp gpio *
+//          chmod g+w *
+//	See:
+////////////////////////////////////////
+const fs = require("fs");
 
-// This is an example of driving a DC motor
+const pwmPeriod = '1000000';    // Period in ns
+const pwmchip = '5';  // pwm chip to use
+const pwm = '1';      // pwm to use
+const PWMPATH='/sys/class/pwm/pwmchip'+pwmchip;
 
-var b = require('bonescript');
+const min = 0.05,     // Slowest speed (duty cycle)
+      max = 1,        // Fastest (always on)
+      ms = 100;       // How often to change speed, in ms
+var   speed = 0.5,    // Current speed;
+      step = 0.05;    // Change in speed
 
-var motor = 'P9_16',// Pin to drive transistor
-    min = 0.05,     // Slowest speed (duty cycle)
-    max = 1,        // Fastest (always on)
-    ms = 100,       // How often to change speed, in ms
-    speed = 0.5,    // Current speed;
-    step = 0.05;    // Change in speed
+// fs.writeFileSync(PWMPATH+'/export', pwm);   // Export the pwm channel
+// Set the period in ns, first 0 duty_cycle, 
+fs.writeFileSync(PWMPATH+'/pwm'+pwm+'/duty_cycle', '0');
+fs.writeFileSync(PWMPATH+'/pwm'+pwm+'/period', pwmPeriod);
+fs.writeFileSync(PWMPATH+'/pwm'+pwm+'/duty_cycle', pwmPeriod/2);
+fs.writeFileSync(PWMPATH+'/pwm'+pwm+'/enable', '1');
 
-b.pinMode(motor, b.ANALOG_OUTPUT, 6, 0, 0, doInterval);
-
-function doInterval(x) {
-    if(x.err) {
-        console.log('x.err = ' + x.err);
-        return;
-    }
-    var timer = setInterval(sweep, ms);
-}
+setInterval(sweep, ms);
 
 function sweep() {
     speed += step;
     if(speed > max || speed < min) {
         step *= -1;
     }
-    b.analogWrite(motor, speed);
+    fs.writeFileSync(PWMPATH+'/pwm'+pwm+'/duty_cycle', 
+    parseInt(pwmPeriod*speed));
     console.log('speed = ' + speed);
 }
 
 process.on('SIGINT', function() {
     console.log('Got SIGINT, turning motor off');
     clearInterval(timer);       // Stop the timer
-    b.analogWrite(motor, 0);    // Turn motor off
+    fs.writeFileSync(PWMPATH+'/pwm'+pwm+'/enable', '0');
 });
