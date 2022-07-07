15f6626b339bba1eae77f3760a70b97e62e12fd4
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2022-07-07 16:30:01 -0400

    Updated setDNS.hs

--- docs/05tips/code/setDNS.sh
+++ docs/05tips/code/setDNS.sh
@@ -3,21 +3,38 @@
 #  can access the Internet through the USB connection.
 # Run ./ipMasquerade.sh the first time. It will set up the host.
 # Run this script if the host is already set up.
+# Inspired by http://thoughtshubham.blogspot.com/2010/03/internet-over-usb-otg-on-beagleboard.html
 
 hostAddr=192.168.7.1
-beagleAddr=192.168.7.2
+beagleAddr=${1:-192.168.7.2}
 
-# Save the /etc/resolv.conf on the Bone in case we mess things up.
+# Save the /etc/resolv.conf on the Beagle in case we mess things up.
 ssh root@$beagleAddr "mv -n /etc/resolv.conf /etc/resolv.conf.orig"
 # Create our own resolv.conf
 cat - << EOF > /tmp/resolv.conf
-# This is installed by host.setDNS.sh on the host
+# This is installed by ./setDNS.sh on the host
 
 EOF
 
+TMP=/tmp/nmcli
 # Look up the nameserver of the host and add it to our resolv.conf
-nmcli dev list | grep IP4.DNS | sed 's/IP4.DNS\[.\]:/nameserver/' >> /tmp/resolv.conf
+# From: http://askubuntu.com/questions/197036/how-to-know-what-dns-am-i-using-in-ubuntu-12-04
+# Use nmcli dev list for older version nmcli
+# Use nmcli dev show for newer version nmcli
+nmcli dev show > $TMP
+if [ $? -ne 0 ]; then   # $? is the return code, if not 0 something bad happened.
+    echo "nmcli failed, trying older 'list' instead of 'show'"
+    nmcli dev list > $TMP
+    if [ $? -ne 0 ]; then
+        echo "nmcli failed again, giving up..."
+        exit 1
+    fi
+fi
+
+grep IP4.DNS $TMP | sed 's/IP4.DNS\[.\]:/nameserver/' >> /tmp/resolv.conf
+
 scp /tmp/resolv.conf root@$beagleAddr:/etc
 
 # Tell the beagle to use the host as the gateway.
 ssh root@$beagleAddr "/sbin/route add default gw $hostAddr" || true
+
