ca6b15eaa11a9acd7db3ca64af545a08001d2f3e
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2022-07-12 17:17:26 -0400

    Removed Adafruit_BBIO

--- docs/06iot/code/flask/app5.py
+++ docs/06iot/code/flask/app5.py
@@ -1,32 +1,50 @@
 #!/usr/bin/env python3
 # From: https://towardsdatascience.com/python-webserver-with-flask-and-raspberry-pi-398423cc6f5d
-'''
-	Beagle GPIO Status and Control - with toggle
-'''
-import Adafruit_BBIO.GPIO as GPIO
+import os
+# import Adafruit_BBIO.GPIO as GPIO
 from flask import Flask, render_template, request
 app = Flask(__name__)
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setwarnings(False)
+GPIOPATH="/sys/class/gpio"
 #define sensors GPIOs
-button = "P9_11"
+button = "30"  # "P9_11"
 #define actuators GPIOs
-ledRed = "P9_14"
+ledRed = "50"  # "P9_14"
+
+# Make sure pin is exported
+if (not os.path.exists(GPIOPATH+"/gpio"+button)):
+    f = open(GPIOPATH+"/export", "w")
+    f.write(pin)
+    f.close()
+if (not os.path.exists(GPIOPATH+"/gpio"+ledRed)):
+    f = open(GPIOPATH+"/export", "w")
+    f.write(pin)
+    f.close()
 #initialize GPIO status variables
 buttonSts = 0
 ledRedSts = 0
 # Define button and PIR sensor pins as an input
-GPIO.setup(button, GPIO.IN)   
+f = open(GPIOPATH+"/gpio"+button+"/direction", "w")
+f.write("in")
+f.close() 
 # Define led pins as output
-GPIO.setup(ledRed, GPIO.OUT)   
+f = open(GPIOPATH+"/gpio"+ledRed+"/direction", "w")
+f.write("out")
+f.close()  
 # turn leds OFF 
-GPIO.output(ledRed, GPIO.LOW)
+f = open(GPIOPATH+"/gpio"+ledRed+"/value", "w")
+f.write("0")
+f.close()
 
 @app.route("/")
 def index():
 	# Read GPIO Status
-	buttonSts = GPIO.input(button)
-	ledRedSts = GPIO.input(ledRed)
+	f = open(GPIOPATH+"/gpio"+button+"/value", "r")
+	buttonSts = f.read()[:-1]
+	f.close()
+	f = open(GPIOPATH+"/gpio"+ledRed+"/value", "r")
+	ledRedSts = f.read()[:-1]
+	f.close()
+
 	templateData = {
       		'button'  : buttonSts,
       		'ledRed'  : ledRedSts,
@@ -38,15 +56,27 @@ def action(deviceName, action):
 	if deviceName == 'ledRed':
 		actuator = ledRed
 
+	f = open(GPIOPATH+"/gpio"+ledRed+"/value", "w")
 	if action == "on":
-		GPIO.output(actuator, GPIO.HIGH)
+		f.write("1")
 	if action == "off":
-		GPIO.output(actuator, GPIO.LOW)
+		f.write("0")
 	if action == "toggle":
-		GPIO.output(actuator, not GPIO.input(actuator))
+		f.close()
+		f = open(GPIOPATH+"/gpio"+ledRed+"/value", "r")
+		ledRedSts = int(f.read()[:-1])
+		ledRedSts = str(int(not ledRedSts))
+		f.close()
+		f = open(GPIOPATH+"/gpio"+ledRed+"/value", "w")
+		f.write(ledRedSts)
+	f.close()
 		     
-	buttonSts = GPIO.input(button)
-	ledRedSts = GPIO.input(ledRed)
+	f = open(GPIOPATH+"/gpio"+button+"/value", "r")
+	buttonSts = f.read()[:-1]
+	f.close()
+	f = open(GPIOPATH+"/gpio"+ledRed+"/value", "r")
+	ledRedSts = int(f.read()[:-1])
+	f.close()
 
 	templateData = {
 	 	'button'  : buttonSts,
@@ -54,4 +84,4 @@ def action(deviceName, action):
 	}
 	return render_template('index5.html', **templateData)
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=8081, debug=True)
+   app.run(host='0.0.0.0', port=8080, debug=True)
