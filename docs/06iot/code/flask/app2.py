eb4ee5a4e6e70feee03b262cf88ea5bb9fd77b9e
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2022-07-12 15:16:48 -0400

    Added flask app2 and removed Adafruit_BBIO.

--- docs/06iot/code/flask/app2.py
+++ docs/06iot/code/flask/app2.py
@@ -1,27 +1,36 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # From: https://towardsdatascience.com/python-webserver-with-flask-and-raspberry-pi-398423cc6f5d
-
-'''
-	Raspberry Pi GPIO Status and Control
-'''
-import Adafruit_BBIO.GPIO as GPIO
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
-button = "P9_11"
-buttonSts = GPIO.LOW
+pin = '30' #  P9_11 is gpio 30
+GPIOPATH="/sys/class/gpio"
+buttonSts = 0
 
-# Set button as an input
-GPIO.setup(button, GPIO.IN)   
+# Make sure pin is exported
+if (not os.path.exists(GPIOPATH+"/gpio"+pin)):
+    f = open(GPIOPATH+"/export", "w")
+    f.write(pin)
+    f.close()
+
+# Make it an input pin
+f = open(GPIOPATH+"/gpio"+pin+"/direction", "w")
+f.write("in")
+f.close()
 
 @app.route("/")
 def index():
 	# Read Button Status
-	buttonSts = GPIO.input(button)
+	f = open(GPIOPATH+"/gpio"+pin+"/value", "r")
+	buttonSts = f.read()[:-1]
+	f.close()
+
+	# buttonSts = GPIO.input(button)
 	templateData = {
       'title' : 'GPIO input Status!',
       'button'  : buttonSts,
       }
 	return render_template('index2.html', **templateData)
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=8081, debug=True)
+   app.run(host='0.0.0.0', port=8080, debug=True)
