42c90486c85d55e9bee40e9008c6b0c54053ef9a
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2022-07-12 15:36:27 -0400

    Added app3

--- docs/06iot/code/flask/app3.py
+++ docs/06iot/code/flask/app3.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # From: https://towardsdatascience.com/python-webserver-with-flask-and-raspberry-pi-398423cc6f5d
-'''
-	Raspberry Pi GPIO Status and Control
-'''
 import Adafruit_BBIO.GPIO as GPIO
 from flask import Flask, render_template, request
 app = Flask(__name__)
 #define LED GPIOs
-ledRed = "P9_15"
+ledRed = "P9_14"
 #initialize GPIO status variable
 ledRedSts = 0
 # Define led pins as output
@@ -42,4 +39,4 @@ def action(deviceName, action):
 	}
 	return render_template('index3.html', **templateData)
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=8081, debug=True)
+   app.run(host='0.0.0.0', port=8080, debug=True)
