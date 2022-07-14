b6e81e8579bad02605380983e6b1f9c8e0d14c95
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2022-07-14 16:44:07 -0400

    Tries at twilio

---
+++ docs/06iot/code/twilioTest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+18122333219',
+                     to='+18122333219'
+                 )
+
+print(message.sid)
