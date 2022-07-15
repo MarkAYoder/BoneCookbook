55670e91b9002568f25ca2b319385706952c85b6
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2022-07-15 12:11:44 -0400

    Switched to my tweets

--- docs/06iot/code/twitter_recent_search.py
+++ docs/06iot/code/twitter_recent_search.py
@@ -12,7 +12,8 @@ search_url = "https://api.twitter.com/2/tweets/search/recent"
 
 # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
 # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
-query_params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev','tweet.fields': 'author_id'}
+query_params = {'query': '(from:MarkAYoder) OR #MarkAYoder','tweet.fields': 'author_id'}
+# query_params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev','tweet.fields': 'author_id'}
 
 
 def bearer_oauth(r):
