4b0d1c6b69be35a5ffd1284bb4bbe5b57c1b75d4
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2022-07-19 10:55:25 -0400

    Removing xenomai

--- docs/02sensors/code/gpiod/toggle1.py
+++ docs/02sensors/code/gpiod/toggle1.py
@@ -3,7 +3,7 @@
 # 	toggle1.py
 #  Toggles the P9_14 pin as fast as it can. P9_14 is line 18 on chip 1.
 # 	Wiring:	Attach an oscilloscope to P9_14 to see the squarewave or 
-#          uncomment the uleep and attach an LED.
+#          uncomment the sleep and attach an LED.
 # 	Setup:	sudo apt update; pip install gpiod
 # 	See:	https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/tree/bindings/python/examples
 # //////////////////////////////////////
