6720745a8b865374aa15c5d25ba54a7a1963dd20
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2022-07-19 15:05:03 -0400

    Removed libsoc

--- docs/08realtime/code/pushLED.c
+++ docs/08realtime/code/pushLED.c
@@ -1,81 +1,66 @@
+////////////////////////////////////////
+//	blinkLED.c
+//	Blinks the P9_14 pin based on the P9_42 pin
+//	Wiring:
+//	Setup:
+//	See:
+////////////////////////////////////////
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
+#define MAXSTR 100
 
-#include "libsoc_gpio.h"
-#include "libsoc_debug.h"
+int main() {
+  FILE *fpbutton, *fpLED;
+  char LED[] = "50";   // Look up P9.14 using gpioinfo | grep -e chip -e P9.14.  chip 1, line 18 maps to 50
+  char button[] = "7"; // Look up P9.42 using gpioinfo | grep -e chip -e P9.42.  chip 0, line 7 maps to 7
+  char GPIOPATH[] = "/sys/class/gpio";
+  char path[MAXSTR] = "";
 
-/**
- * 
- * This gpio_test is intended to be run on BeagleBone
- * and reads pin P9_42 (gpio7) and write the value to P9_12 (gpio60).
- *
- * The GPIO_OUTPUT and INPUT defines can be changed to support any two pins.
- * 
- */
- 
-#define GPIO_OUTPUT  60
-#define GPIO_INPUT   7
-
-// Create both gpio pointers
- gpio *gpio_output, *gpio_input;
-
-static int interrupt_count = 0;
-
-int callback_test(void* arg) {
-  int* tmp_count = (int*) arg;
-  int value;
-  
-  *tmp_count = *tmp_count + 1;
-  value = libsoc_gpio_get_level (gpio_input);
-  libsoc_gpio_set_level(gpio_output, value);
-  // Comment out the following line to make the code respond faster
-  printf("Got it (%d), button = %d\n", *tmp_count, value);
-  
-  return EXIT_SUCCESS;
-}
-
-int main(void) {
-  // Enable debug output
-  libsoc_set_debug(1);
-
-  // Request gpios
-  gpio_output = libsoc_gpio_request(GPIO_OUTPUT, LS_SHARED);
-  gpio_input = libsoc_gpio_request(GPIO_INPUT, LS_SHARED);
-
-  // Set direction to OUTPUT
-  libsoc_gpio_set_direction(gpio_output, OUTPUT);
-  
-  // Set direction to INPUT
-  libsoc_gpio_set_direction(gpio_input, INPUT);
-
-  // Set edge to BOTH
-  libsoc_gpio_set_edge(gpio_input, BOTH);
-
-  // Set up callback
-  libsoc_gpio_callback_interrupt(gpio_input, &callback_test, 
-                      (void*) &interrupt_count);
-  
-  printf("Push the button...\n"); 
-  // Disaple debug output so the code will respond faster
-  libsoc_set_debug(0);
-
-  sleep(10);
-  
-  libsoc_set_debug(1);
- 
-  // Cancel the callback on interrupt
-  libsoc_gpio_callback_interrupt_cancel(gpio_input);
-  
-  //If gpio_request was successful
-  if (gpio_input) { // Free gpio request memory
-    libsoc_gpio_free(gpio_input);
+  // Make sure LED is exported
+  snprintf(path, MAXSTR, "%s%s%s", GPIOPATH, "/gpio", LED);
+  if (!access(path, F_OK) == 0) {
+    snprintf(path, MAXSTR, "%s%s", GPIOPATH, "/export");
+    fpLED = fopen(path, "w");
+    fprintf(fpLED, "%s", LED);
+    fclose(fpLED);
   }
-  
-  if (gpio_output) { // Free gpio request memory
-    libsoc_gpio_free(gpio_output);
+ 
+  // Make it an output LED
+  snprintf(path, MAXSTR, "%s%s%s%s", GPIOPATH, "/gpio", LED, "/direction");
+  fpLED = fopen(path, "w");
+  fprintf(fpLED, "out");
+  fclose(fpLED);
+
+  // Make sure bbuttonutton is exported
+  snprintf(path, MAXSTR, "%s%s%s", GPIOPATH, "/gpio", button);
+  if (!access(path, F_OK) == 0) {
+    snprintf(path, MAXSTR, "%s%s", GPIOPATH, "/export");
+    fpbutton = fopen(path, "w");
+    fprintf(fpbutton, "%s", button);
+    fclose(fpbutton);
   }
-  
-  return EXIT_SUCCESS;
+ 
+  // Make it an input button
+  snprintf(path, MAXSTR, "%s%s%s%s", GPIOPATH, "/gpio", button, "/direction");
+  fpbutton = fopen(path, "w");
+  fprintf(fpbutton, "in");
+  fclose(fpbutton);
+
+  // Read every .25 sec
+  // snprintf(path, MAXSTR, "%s%s%s%s", GPIOPATH, "/gpio", button, "/value");
+  // fpbutton = fopen(path, "r");
+  snprintf(path, MAXSTR, "%s%s%s%s", GPIOPATH, "/gpio", LED,    "/value");
+  fpLED    = fopen(path, "w");
+  char state = '0';
+  while (1) {
+    snprintf(path, MAXSTR, "%s%s%s%s", GPIOPATH, "/gpio", button, "/value");
+    fpbutton = fopen(path, "r");
+    fseek(fpLED,    0L, SEEK_SET);
+    fscanf(fpbutton, "%c", &state);
+    printf("state: %c\n", state);
+    fprintf(fpLED, "%c", state);
+    fclose(fpbutton);
+    usleep(1000000);   // sleep time in microseconds
+  }
 }
